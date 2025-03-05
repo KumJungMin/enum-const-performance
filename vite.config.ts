cf2737e7a48cdcabb58bcda8c25ba6704bf8068a
KumJungMin <rmawjdals@naver.com>
2025-03-05 19:11:52 +0900

    chore: add rollup-plugin-visualizer for bundle visualization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true })]
+    }
+  },
 })
